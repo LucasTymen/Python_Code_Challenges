55a73bb7f561555ca63fd49b9ece455558efda66
Lucas Tymen <lucas.tymen@gmail.com>
2023-05-16 17:27:59 +0200

    code challenges_strings_3

---
+++ strings/03_Count_Multi_X/multy_x.py
@@ -0,0 +1,16 @@
+# Write your count_multi_char_x function here:
+def count_multi_char_x (word,x):
+  counter = 0
+  for x in word.split(x):
+    counter += 1
+  return counter -1
+# Uncomment these function calls to test your function:
+print(count_multi_char_x("mississippi", "iss"))
+# should print 2
+print(count_multi_char_x("apple", "pp"))
+# should print 1
+
+# ===========>>>>>> solution
+def count_multi_char_x(word, x):
+  splits = word.split(x)
+  return(len(splits)-1)
