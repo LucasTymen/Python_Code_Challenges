4ecbf2c0dd5794843b35c0f2b4fe51924c0857fb
Lucas Tymen <lucas.tymen@gmail.com>
2023-05-16 16:12:42 +0200

    code challenges

---
+++ strings/pitch.py
@@ -0,0 +1,43 @@
+"""
+Python Code Challenges: Strings
+
+Python Code Challenges involving Strings
+
+This article will help you review Python functions by providing some code challenges about strings.
+
+Some of these challenges are difficult! Take some time to think about them before starting to code.
+
+You might not get the solution correct on your first try — look at your output, try to find where you’re going wrong, and iterate on your solution.
+
+Finally, if you get stuck, use our solution code! If you “Check Answer” twice with an incorrect solution, you should see an option to get our solution code. However, truly investigate that solution — experiment and play with the solution code until you have a good grasp of how it is working. Good luck!
+Function Syntax
+
+As a refresher, function syntax looks like this:
+"""
+def some_function(some_input1, some_input2):
+  # … do something with the inputs …
+  return output
+"""
+For example, a function that finds the difference in length between two Strings would look like this:
+"""
+def lengthDiff(str1, str2):
+  return len(str1) - len(str2)
+"""
+And this would produce output like:
+"""
+lengthDiff("Python", "rocks")
+"""
+1
+"""
+lengthDiff("Marco", "Polo")
+"""
+1
+"""
+lengthDiff("Kevin", "Durant")
+"""
+-1
+
+Challenges
+
+We’ve included 5 challenges below. Try to answer all of them and polish up your problem-solving skills and your string expertise.
+"""
