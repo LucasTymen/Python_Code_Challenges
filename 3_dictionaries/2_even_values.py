9b5649f7b792ca315bb86a3a5b8968d12c2bb16d
Lucas Tymen <lucas.tymen@gmail.com>
2023-05-17 14:13:36 +0200

    code challenges_dictionaries_2

--- 3_dictionaries/2_even_values.py
+++ 3_dictionaries/2_even_values.py
@@ -11,3 +11,9 @@ print(sum_even_keys({1:5, 2:2, 3:3}))
 # should print 2
 print(sum_even_keys({10:1, 100:2, 1000:3}))
 # should print 6
+
+"""
+Similar to the previous problem, we are iterating through our dictionary, except this time we are iterating through the keys instead of the values.
+In order to get the keys we use the keys() function and to get the value of a key we can use brackets.
+To test if the key is even we use the modulus operator and test if the remainder is 0 when dividing by 2.
+"""
