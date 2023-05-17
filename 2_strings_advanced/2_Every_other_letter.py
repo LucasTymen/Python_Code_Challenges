f8a7947899c9361e079cebfd613cca91174e1da3
Lucas Tymen <lucas.tymen@gmail.com>
2023-05-17 10:43:31 +0200

    code challenges_strings Advanced_2

--- 2_strings_advanced/2_Every_other_letter.py
+++ 2_strings_advanced/2_Every_other_letter.py
@@ -1,4 +1,9 @@
 # Write your every_other_letter function here:
+def every_other_letter(word):
+  every_other = ""
+  for i in range(0, len(word), 2):
+    every_other += word[i]
+  return every_other
 
 # Uncomment these function calls to test your function:
 print(every_other_letter("Codecademy"))
