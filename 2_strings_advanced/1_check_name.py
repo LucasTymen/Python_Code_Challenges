65a3ba092ba52afea47f2351c21afbe8e076d545
Lucas Tymen <lucas.tymen@gmail.com>
2023-05-17 10:46:16 +0200

    code challenges_strings Advanced_2

--- 2_strings_advanced/1_check_name.py
+++ 2_strings_advanced/1_check_name.py
@@ -9,3 +9,10 @@ print(check_for_name("My name is jamie", "Jamie"))
 # should print True
 print(check_for_name("My name is Samantha", "Jamie"))
 # should print False
+
+"""
+As you can see, this function can be created using one line.
+The in keyword will return True if the first provided string is within the second.
+So in this case, we’re checking if name is in sentence.
+In order to ignore differences in capitalization, we can use the .lower() function which converts all characters to lowercase characters.
+"""
--- 2_strings_advanced/2_Every_other_letter.py
+++ 2_strings_advanced/2_Every_other_letter.py
@@ -12,3 +12,12 @@ print(every_other_letter("Hello world!"))
 # should print Hlowrd
 print(every_other_letter(""))
 # should print
+
+"""
+In order to alternate which character is added to the every_other string, we use a range of indices which starts at index 0 (the beginning of our word) and ends at the end of our word.
+The third parameter in the range function determines what value to increment by.
+In this case, we want to increment by 2 in order to get every other letter.
+
+Another way to loop through all indices of our original string, but only add the letters that correspond to an even index.
+As a challenge, try solving this problem that way!
+"""
